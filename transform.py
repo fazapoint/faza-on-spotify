bef6a50fa4d1dcc07459cbb97f1c9f2e5f164109
Anisykurli Faza Ramadhani <fazaramadhani99@gmail.com>
2026-05-01 21:29:45 +0700

    adjust query transform

--- transform.py
+++ transform.py
@@ -13,8 +13,12 @@ WITH raw_data as (
 SELECT
     entry.played_at,
     entry.track.name as song_name,
-    entry.track.artists[1].name as artist_name,
-    entry.track.album.name
+    entry.track.duration_ms as song_duration,
+    entry.track.artists[1].name as artist_name, --need to adjust if theres more than one artists
+    entry.track.album.name as album_name,
+    entry.track.album.release_date as album_release_date,
+    entry.track.album.album_type as album_type,
+    entry.track.album.images[2].url as album_cover_url
 FROM raw_data
 """
 
